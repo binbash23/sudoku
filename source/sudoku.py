a338917b82388693a4f676dfe38d181f0d300dcd
Jens Heine <binbash@gmx.net>
2024-10-16 14:24:45 +0200

    Highlight sectors with 0 or 1 null(s)

--- source/sudoku.py
+++ source/sudoku.py
@@ -97,7 +97,12 @@ def count_nulls_in_sector(a: array, sector: int, verbose: bool=False) -> int:
                 if a[x, y] == N:
                     count_nulls += 1
     if verbose:
-        print("Nulls in S" + str(sector) + " : " + str(count_nulls))
+        if count_nulls == 0 or count_nulls == 1:
+            print("Nulls in S" + str(sector + 1) + " : " + str(count_nulls) + " *")
+        else:
+            print("Nulls in S" + str(sector + 1) + " : " + str(count_nulls))
+    # if verbose:
+    #     print("Nulls in S" + str(sector) + " : " + str(count_nulls))
     return count_nulls
 
 
