8a99802df129b1c2a6a567cdde29acc35fb55a16
Jens Heine <binbash@gmx.net>
2024-10-18 17:38:49 +0200

    Add function documentation

--- source/branch_point.py
+++ source/branch_point.py
@@ -12,8 +12,8 @@ from numpy import array
 from uuid import uuid4
 import copy
 
-class BranchPoint:
 
+class BranchPoint:
     _a: array = None
     _is_root: bool = False
     _branch_position_row_index: int = -1
@@ -23,11 +23,24 @@ class BranchPoint:
     _id: uuid4 = None
     _parent_id = None
 
-
-    def __init__(self, a: array, branch_position_row_index: int = -1, branch_position_column_index = -1,
-                 is_root: bool = False, initial_possibilities:array=array([]),
-                 # possibilities_left:array=array([]),
-                 parent_id: uuid4=None):
+    def __init__(self, a: array, branch_position_row_index: int = -1, branch_position_column_index=-1,
+                 is_root: bool = False, initial_possibilities: array = array([]),
+                 parent_id: uuid4 = None):
+        """
+        The constructor creates a new branch_point object, which can be a root branch point or a non-root
+        branch point. The non-root branch point holds information like a copy of the sudoku matrix at
+        creation time. Also the best position identified by row_index and column_index to go on trying.
+        The possible numbers are remembered, too. The parent id points to the parent branch point of this
+        one, to be able to follow the tree up to the root branch point from any leaf. It is possible to
+        pop out one possible number after another to try one solution number and get back if this was not
+        the right one - and then pop out the next number to try.
+        :param a: A two-dimensional array with the sudoku data
+        :param branch_position_row_index: row index of the position to dive into
+        :param branch_position_column_index: column index of the position to dive into
+        :param is_root: True if this should be the root branch point
+        :param initial_possibilities: initial possible numbers on the position to try
+        :param parent_id: The id of the parent branch point
+        """
         self._a = numpy.copy(a)
         self._is_root = is_root
         self._branch_position_row_index = branch_position_row_index
@@ -37,9 +50,12 @@ class BranchPoint:
         self._id = uuid4()
         self._parent_id = parent_id
 
-
     # def __str__(self):
     def print(self):
+        """
+        Print out the information of this branch point
+        :return:
+        """
         print("Sudoku branch point")
         print()
         print("Sudoku matrix:")
@@ -48,55 +64,85 @@ class BranchPoint:
         print("Parent ID              : " + str(self._parent_id))
         print("ID                     : " + str(self._id))
         print("Root                   : " + str(self._is_root))
-        print("Branch position row    : " + str(self._branch_position_row_index))
-        print("Branch position column : " + str(self._branch_position_column_index))
+        print("Branch position row    : " + str(self.get_branch_position_row()))
+        print("Branch position column : " + str(self.get_branch_position_column()))
         print("Initial possibilities  : " + str(self._initial_possibilities))
         print("Possibilities left     : " + str(self._possibilities_left))
 
-
     def get_branch_position_row(self) -> int:
+        """
+        Get the branch position row number (1-9) or -1 if not set.
+        :return:
+        """
         if self._branch_position_row_index > -1:
-            return self._branch_position_row_index+1
+            return self._branch_position_row_index + 1
         else:
             return -1
-
 
     def get_branch_position_column(self) -> int:
+        """
+        Get the branch position column number (1-9) or -1 if not set.
+        :return:
+        """
         if self._branch_position_column_index > -1:
-            return self._branch_position_column_index+1
+            return self._branch_position_column_index + 1
         else:
             return -1
 
-
     def get_initial_possibilities(self) -> array:
+        """
+        Get the array with the initial possible numbers of the position which can be tried
+        our from this branch point.
+        :return:
+        """
         return self._initial_possibilities
 
-
     def get_possibilities_left(self) -> array:
+        """
+        Get the array with the numbers left to try for the position of this branch point
+        which ave not been tried out.
+        :return:
+        """
         return self._possibilities_left
 
-
     def get_id(self) -> uuid4:
+        """
+        Get the id of this branch point.
+        :return: the id of the branch point
+        """
         return self._id
 
-
     def has_possibilities_left(self) -> bool:
+        """
+        Return True if there are some possible numbers left to try out.
+        :return: True if there are some un-tried numbers left
+        """
         if len(self.get_possibilities_left()) > 0:
             return True
         else:
             return False
 
-
     def is_root(self) -> bool:
+        """
+        Checks if this branch point is the root branch point.
+        :return: True if this branch point is the root
+        """
         return self._is_root
 
-
     def get_parent_id(self) -> uuid4:
+        """
+        Get the id of the parent branch point if it exists.
+        :return: The id of the parent branch point if this is not the root
+        """
         return self._parent_id
 
-
-    def pop_possibility_left(self) -> int|None:
-        possibility=None
+    def pop_possible_number(self) -> int | None:
+        """
+        Pop the next possible number that can be tried out for the position of this branch point.
+        The number will be removed from the list of possible numbers.
+        :return: A possible number that can be tried or None
+        """
+        possibility = None
         if self.has_possibilities_left():
             possibility = self._possibilities_left[0]
             self._possibilities_left = numpy.delete(self._possibilities_left, 0)
@@ -114,7 +160,7 @@ def main():
     #bp_array = array([bp])
     #bp_array = array([])
     #bp_array = bp_array + [copy.deepcopy(bp)]
-    bp_array = array([ copy.deepcopy(bp)])
+    bp_array = array([copy.deepcopy(bp)])
     bp_array = [copy.deepcopy(bp)]
 
     print("==")
@@ -129,4 +175,3 @@ if __name__ == '__main__':
     main()
 
     # def get_parent_branch_point(branch_point: SudokuBranchPoint, branch_point_array: array([SudokuBranchPoint]) -> SudokuBranchPoint:
-
--- source/branch_point_tree.py
+++ source/branch_point_tree.py
@@ -5,7 +5,6 @@
 branch_point_tree.py
 
 """
-import numpy
 from numpy import array
 from branch_point import BranchPoint
 from uuid import uuid4
@@ -16,11 +15,21 @@ class BranchPointTree:
 
 
     def __init__(self, root_array: array):
+        """
+        Create a branch_point_tree object. The root branch point object will be generated from
+        the given array.
+        :param root_array: The root sudoku array
+        """
         root_branch_point = BranchPoint(root_array, is_root=True)
         self._branch_point_array = self._branch_point_array + [root_branch_point]
 
 
     def print(self):
+        """
+        Print out some information about this branch point tree object which also
+        lists all contained branch point information.
+        :return:
+        """
         print()
         print("Branch point tree (" + str(self.get_size()) + " elements)")
         i=0
@@ -34,14 +43,28 @@ class BranchPointTree:
 
 
     def add_branch_point(self, branch_point: BranchPoint):
+        """
+        Add a new branch point object to this tree
+        :param branch_point: The new branch point
+        :return:
+        """
         self._branch_point_array = self._branch_point_array + [branch_point]
 
 
     def get_size(self) -> int:
+        """
+        Return the number of branch points in the tree.
+        :return: The number of branch points in this tree
+        """
         return len(self._branch_point_array)
 
 
     def get_branch_point(self, branch_point_id: uuid4) -> BranchPoint|None:
+        """
+        Get a branch point by id.
+        :param branch_point_id: The id to be returned
+        :return: If found, the branch point with the given id
+        """
         i=0
         while i < self.get_size():
             if self._branch_point_array[i].get_id() == branch_point_id:
@@ -51,11 +74,20 @@ class BranchPointTree:
 
 
     def get_parent_of(self, branch_point: BranchPoint) -> BranchPoint|None:
+        """
+        Get the parent branch point of the given branch point.
+        :param branch_point: The child branch point to get the parent from
+        :return: The parent branch point if it exists or None
+        """
         parent_id = branch_point.get_parent_id()
         return self.get_branch_point(parent_id)
 
 
     def get_root(self) -> BranchPoint|None:
+        """
+        Get the root branch point of this tree.
+        :return: The root branch point
+        """
         i=0
         while i < self.get_size():
             if self._branch_point_array[i].is_root():
